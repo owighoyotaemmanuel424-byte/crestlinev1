c87dc860d636853cad2c88c05ee20bf6fe6bff50
owighoyotaemmanuel424-byte <owighoyotaemmanuel424@gmail.com>
2026-09-13 01:56:40 +0100

    Add index.ts barrel export to API client layer

---
+++ src/lib/api/index.ts
@@ -0,0 +1,22 @@
+// src/lib/api/index.ts
+// API Client Index - Export all API modules
+
+export { apiClient, ApiClient, ApiResponse, ApiError } from './client';
+export { accountsApi } from './accounts';
+export { transactionsApi } from './transactions';
+export { transfersApi } from './transfers';
+export { depositsApi } from './deposits';
+export { withdrawalsApi } from './withdrawals';
+export { usersApi } from './users';
+export { notificationsApi } from './notifications';
+export { beneficiariesApi } from './beneficiaries';
+
+// Re-export types
+export type { Account, AccountListParams, AccountListResult, CreateAccountData, UpdateAccountData } from './accounts';
+export type { Transaction, TransactionListParams, TransactionListResult } from './transactions';
+export type { Transfer, TransferListParams, TransferListResult, CreateTransferData } from './transfers';
+export type { Deposit, DepositListParams, DepositListResult, CreateDepositData } from './deposits';
+export type { Withdrawal, WithdrawalListParams, WithdrawalListResult, CreateWithdrawalData } from './withdrawals';
+export type { User, ProfileUpdateData, PasswordUpdateData } from './users';
+export type { Notification, NotificationListParams, NotificationListResult } from './notifications';
+export type { Beneficiary, BeneficiaryListParams, BeneficiaryListResult, CreateBeneficiaryData, UpdateBeneficiaryData } from './beneficiaries';
