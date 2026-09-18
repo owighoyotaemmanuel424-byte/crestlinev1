007857bebbd0bfbe5e9c0147fb9a35f15c871d10
owighoyotaemmanuel424-byte <owighoyotaemmanuel424@gmail.com>
2026-09-18 23:44:40 +0100

    Update errors.ts

--- src/lib/utils/errors.ts
+++ src/lib/utils/errors.ts
@@ -74,8 +74,7 @@ export class TransferError extends AppError {
 
 export class InvalidRecipientError extends AppError {
   constructor(message: string) {
-    super(400, message, 'INVALID
-_RECIPIENT');
+    super(400, message, 'INVALID_RECIPIENT');
   }
 }
 
@@ -161,8 +160,7 @@ export class WebhookError extends AppError {
 // State Machine Errors
 export class InvalidStateTransitionError extends AppError {
   constructor(from: string, to: string) {
-    super(400, `Cannot transition from ${from} to ${to}`, 'INVALID
-_STATE_TRANSITION');
+    super(400, `Cannot transition from ${from} to ${to}`, 'INVALID_STATE_TRANSITION');
   }
 }
 
@@ -172,7 +170,6 @@ export class OwnershipError extends AppError {
     super(403, `You do not own this ${resource}`, 'OWNERSHIP_ERROR');
   }
 }
-
 
 // Limit Errors
 export class DailyLimitExceededError extends AppError {
